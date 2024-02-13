0e1917419ea308519b59c52b6bb58e672e6e66c8
Michael Romer <mvromer@gmail.com>
2024-02-13 00:05:48 -0600

    Refactor to include NPM helper package (#7)
    * Drop .NET 7 support
    
    * Temporarily remove ASP.NET Core 8.x submodule
    
    * Move NuGet project
    
    * Add blazor-wasm-single-spa package
    
    * Update workflow
    
    * README and API updates

--- README.md
+++ README.md
@@ -1,53 +1,177 @@
 # Blazor.WebAssembly.SingleSpa
 
 Proof of concept around enabling the integration of Blazor WASM applications as micro frontends
-targeting single-spa
+targeting single-spa. This integration comprises two packages:
 
-> 🚨 **NOTE** 🚨
->
-> In the near future, .NET 8 will be the only version of .NET targeted by this experimental package.
-> The .NET 7 specific versions will be dropped. Also, this README is in need of some major updates.
-> They will come once the work on supporting .NET 8 has settled.
+* Blazor.WebAssembly.SingleSpa &ndash; NuGet package that defines an alternate Blazor WebAssembly
+  *start script* that allows a WASM application to be used as a micro-frontend in a single-spa
+  web application.
+* blazor-wasm-single-spa &ndash; NPM package that defines a `singleSpaBlazor` helper function for
+  defining the lifecycle hooks single-spa expects each micro-frontend to provide for bootstrapping,
+  mounting, and unmounting.
 
 ## Applicable Versions
 
-The Blazor.WebAssembly.SingleSpa NuGet package contains experimental support for projects targeting
-.NET 7 and .NET 8. Specifically, the assets for each target framework version were built from the
-following specific versions of ASP.NET Core:
+The Blazor.WebAssembly.SingleSpa NuGet package provides experimental support for projects targeting
+the versions of .NET listed in the table below. Each entry in the table specifies the exact tagged
+version of ASP.NET Core from which the assets were built.
 
 Target Framework Version | ASP.NET Core Version
 -------------------------|---------------------
-.NET 7                   | 7.0.15
 .NET 8                   | 8.0.1
 
-For a given target framework version, this package _may_ work for previous minor/patch releases.
-However, no guarantees are given.
+For a given target framework version, the Blazor.WebAssembly.SingleSpa package _may_ work for
+previous minor/patch releases. However, no guarantees are given.
 
 ## Usage
 
-In general, installing this package in your Blazor WebAssembly project is sufficient for getting the
-modified Blazor startup script:
+In general, installing the Blazor.WebAssembly.SingleSpa NuGet package in your Blazor WebAssembly
+client project is sufficient for getting the modified Blazor startup script:
 
 ```powershell
 dotnet add package Blazor.WebAssembly.SingleSpa
 ```
 
-However, to fully integrate a Blazor-based micro frontend, you will need to, at the very least,
-supply implementations for the three core single-spa lifecycle callbacks
-([bootstrap](https://single-spa.js.org/docs/building-applications#bootstrap),
+The modified startup script (`blazor.webassembly.js`) augments the `window.Blazor` object created by
+the Blazor runtime to expose additional APIs that make it possible to mount and unmount a Blazor
+WASM application onto and from the DOM. However, to fully integrate and enable within a single-spa
+web application, the Blazor WASM micro-frontend must also supply three lifecycle hooks single-spa
+expects to import. These hooks are
+[bootstrap](https://single-spa.js.org/docs/building-applications#bootstrap),
 [mount](https://single-spa.js.org/docs/building-applications#mount), and
-[unmount](https://single-spa.js.org/docs/building-applications#unmount)). How you do this is up to
-you and your project's needs. As part of this proof of concept, I have put together a simple web
-app built on single-spa that incorporates both [Lit-based](https://lit.dev/) and Blazor-based
-micro frontends. See here: [blazing-lit-mfe-demo](https://github.com/mvromer/blazing-lit-mfe-demo)
+[unmount](https://single-spa.js.org/docs/building-applications#unmount).
 
-## Building from source
+As a convenience, the blazor-wasm-single-spa NPM package exports a helper function for defining
+these lifecycle hooks. The helper is modeled after many of the
+[official single-spa framework helpers](https://single-spa.js.org/docs/ecosystem) available for many
+popular JavaScript frameworks.
+
+To use it, define a script inside your Blazor WASM application's `wwwroot` folder, e.g.,
+`lifecycles.js`. Inside it, you could do the following to export the necessary lifecycle hooks:
+
+```javascript
+import singleSpaBlazor from 'blazor-wasm-single-spa';
+
+export const { bootstrap, mount, unmount } = singleSpaBlazor({
+  // This is the name of a custom element that, when added to the DOM, will trigger
+  // Blazor to render your application's main app component. This custom element is
+  // registered in your Blazor WASM's application by calling the following:
+  //
+  // builder.RootComponents.RegisterCustomElement<App>("my-blazor-app");
+  //
+  // For more information, read here:
+  // https://learn.microsoft.com/en-us/aspnet/core/blazor/components/js-spa-frameworks?view=aspnetcore-8.0#blazor-webassembly-registration
+  //
+  appTagName: 'my-blazor-app',
+
+  // Base URL from which assets (JS, CSS, .NET WASM, etc. files) for your Blazor
+  // web application will be fetched. This is typically the URL that is hosting your
+  // Blazor WASM micro-frontend. The URL must end in a trailing slash.
+  //
+  // NOTE: In .NET 8, some of the resource fetching code that previously lived in
+  // the Blazor WASM startup script now lives in the .NET browser runtime. This
+  // makes it hard to ensure the correct credential option is set for fetch calls
+  // made by the runtime. In some cases it will set the fetch credentials option
+  // to 'include', and in other cases it will set it to 'same-origin'.
+  //
+  // If your Blazor WASM micro-frontend is hosted at an origin that requires
+  // credentials (e.g., auth cookies), then you may run into CORS issues depending
+  // on how you have configured the Access-Control-Allow-Origin response header
+  // and/or if the .NET browser runtime's fetch requests do not include the
+  // necessary credentials by default.
+  //
+  // In the latter case, it may be possible to override the resource loading
+  // behavior by supplying a custom loadBootResource function. However, this
+  // scenario has not been explicitly tested.
+  //
+  // A more robust solution, especially in the context of an enterprise application,
+  // would be to serve the single-spa application and your micro-frontends through a
+  // reverse proxy hosted on the same domain. This would help ensure that all
+  // requests from the browser for micro-frontend assets appear to be same-origin,
+  // thus circumventing potential CORS issues.
+  assetBaseUrl: 'https://mysite.com/apps/my-blazor-app/',
+
+  // Client-side base URL the configure on the Blazor WASM application's navigation
+  // manager. This will be the base URL the navigation manager uses for internal
+  // routing decisions.
+  //
+  // Typically, you would set this to the base URL single-spa uses to decide if your
+  // micro-frontend should be active or not. For example, if single-spa is
+  // configured to activate your Blazor WASM application when the client-side URL
+  // starts with https://mysite.com/blazor-app/, then that same URL is set on the
+  // navigationBaseUrl property.
+  navigationBaseUrl: 'https://mysite.com/blazor-app/',
+
+  // Optional paths to additional stylesheets to include when the Blazor WASM
+  // micro-frontend is mounted by single-spa. These paths are relative to the asset
+  // base URL. For each path given, a link tag referencing the stylesheet is
+  // rendered before the Blazor WASM app's custom element.
+  stylePaths: ['css/app.css', 'MyBlazorApp.Client.styles.css'],
+
+  // Optional additional paths to JavaScript modules that will be dynamically
+  // imported before the .NET browser runtime and Blazor WASM application are
+  // started. These paths are relative to the asset base URL.
+  additionalImportPaths: ['my-js-interop.js'],
+
+  // Optional callback called before the .NET browser runtime and Blazor WASM
+  // application are started. This receives the asset base URL as its argument. Can
+  // return a promise or nothing.
+  beforeBlazorStart: (assetBaseUrl) => { },
+
+  // Following the first mount, this optional callback is called after the Blazor
+  // WASM application's global state (e.g., window.Blazor, window.DotNet, etc.) is
+  // restored on the DOM just before single-spa mounts it. This receives the Blazor
+  // object as its argument. Can return a promise or nothing.
+  afterBlazorRestore: (blazor) => { },
+
+  // Optional callback called after the Blazor WASM application has been removed
+  // from the DOM, its components have been disposed, and its global state (e.g.,
+  // window.Blazor, window.DotNet, etc.) cleared from the window object. This
+  // receives the Blazor object as its argument. Can return a promise or nothing.
+  afterBlazorClear: (blazor) => { },
+
+  // Optional callback that allows additional configuration of the .NET WebAssembly
+  // host. Receives the DotnetHostBuilder and the set of props passed by single-spa
+  // to the Blazor WASM micro-frontend's mount lifecycle hook. Returns nothing.
+  configureRuntime: (dotnet, props) => {
+    dotnet.withEnvironmentVariables({
+        MY_CONFIG_VALUE: 42,
+    });
+  },
+
+  // Optional value that, when true, will render a script tag that injects the
+  // ASP.NET Core browser refresh script. This script is required to have .NET
+  // hot reload work correctly.
+  injectRefreshScript: false,
+
+  // Optional set of "app extensions" that can be used to provide micro-frontend
+  // support for additional Blazor WASM components (e.g., MudBlazor). Each app
+  // extension is an object that defines the following optional properties:
+  //
+  // * stylePaths
+  // * additionalImportPaths
+  // * beforeBlazorStart
+  // * afterBlazorRestore
+  // * afterBlazorClear
+  //
+  // These properties are defined the same as those previously mentioned.
+  appExtensions: [],
+});
+```
+
+As part of this proof of concept, I have put together a simple web app built on single-spa that
+incorporates both [Lit-based](https://lit.dev/) and Blazor-based micro-frontends. See here:
+[blazing-lit-mfe-demo](https://github.com/mvromer/blazing-lit-mfe-demo)
+
+## Building Blazor.WebAssembly.SingleSpa from source
+
+The following assumes you are working from the `Blazor.WebAssembly.SingleSpa` directory.
 
 This repository is structured around the following idea: for a given major version `N` of ASP.NET
-Core, a submodule named `aspnetcore/N.x` is located at `src/aspnetcore/N.x`, Each submodule is
+Core, a submodule named `aspnetcore/N.x` is located at `src/aspnetcore/N.x`. Each submodule is
 configured to pull a version tag from the `dotnet/aspnetcore` repository matching the specified
-major version number, e.g., `src/aspnetcore/7.x` pulls the commit of `dotnet/aspnetcore` tagged with
-`v7.0.15`.
+major version number, e.g., `src/aspnetcore/8.x` pulls the commit of `dotnet/aspnetcore` tagged with
+`v8.0.1`.
 
 Within this project's `patches` directory are a corresponding set of patches and scripts, one for
 each major version of ASP.NET Core that has been tested. The `Build-PatchedBlazorWasm.ps1` script in
---
+++ blazor-wasm-single-spa/src/index.ts
@@ -0,0 +1,376 @@
+import type { AppProps, LifeCycleFn } from "single-spa";
+
+// NOTE: Ideally we would be able to use the types Microsoft defines for things like the Blazor
+// runtime object and the .NET host builder. However, Microsoft doesn't expose them in an easily
+// consumable package. Thus, in a lot of cases these types are declared as any.
+
+// Utility type for getting the parameter type from a function that takes a single parameter.
+type Parameter<T extends (arg: any) => any> = T extends (arg: infer U) => any ? U : never;
+
+// App extensions allow other third-party or shared libraries hook into a Blazor micro-frontend's
+// lifecycle hooks at specific points. Extensions can be used for, e.g., importing and initializing
+// other assets. They can also be used to restore and clear DOM state as needed when the Blazor
+// micro-frontend mounts and unmounts, respectively.
+export type BlazorWebAssemblyAppExtension = {
+  stylePaths?: string[];
+  additionalImportPaths?: string[];
+  beforeBlazorStart?: (assetBaseUrl: URL) => void | Promise<void>;
+  afterBlazorRestore?: (blazor: any) => void | Promise<void>;
+  afterBlazorClear?: (blazor: any) => void | Promise<void>;
+};
+
+// The options for configuring a Blazor WebAssembly micro-frontend's lifecycle hooks.
+export type BlazorWebAssemblyOptions<ExtraProps = {}> = {
+  appTagName: string;
+  stylePaths?: string[];
+  additionalImportPaths?: string[];
+  beforeBlazorStart?: (assetBaseUrl: URL) => void | Promise<void>;
+  afterBlazorRestore?: (blazor: any) => void | Promise<void>;
+  afterBlazorClear?: (blazor: any) => void | Promise<void>;
+  assetBaseUrl?: URL;
+  navigationBaseUrl?: URL;
+  configureRuntime?: (
+    dotnetHostBuilder: any,
+    props: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+  ) => void;
+  injectRefreshScript?: boolean;
+  appExtensions?: BlazorWebAssemblyAppExtension[];
+
+  // This is a common way other single-spa framework helpers allow for customizing the DOM element
+  // onto which the micro-frontend is mounted.
+  domElementGetter?: (props: AppProps) => HTMLElement;
+};
+
+// These are props we know how to handle if they are present in the props passed by single-spa into
+// our lifecycle hooks. ExtraProps are any additional props that the Blazor micro-frontend expects
+// to define and use.
+type BlazorWebAssemblyProps<ExtraProps = {}> = ExtraProps & {
+  assetBaseUrl?: URL;
+  navigationBaseUrl?: URL;
+  domElement?: HTMLElement;
+  domElementGetter?: (props: AppProps) => HTMLElement;
+};
+
+// The type of a lifecycle hook we produce with singleSpaBlazor.
+type BlazorWebAssemblyLifeCycleFn<ExtraProps> = LifeCycleFn<BlazorWebAssemblyProps<ExtraProps>>;
+
+// The set of lifecycle hooks we produce with singleSpaBlazor.
+type BlazorWebAssemblyLifeCycles<ExtraProps> = {
+  bootstrap: BlazorWebAssemblyLifeCycleFn<ExtraProps>;
+  mount: BlazorWebAssemblyLifeCycleFn<ExtraProps>;
+  unmount: BlazorWebAssemblyLifeCycleFn<ExtraProps>;
+};
+
+// This is a template that gets built from the given BlazorWebAssemblyOptions. It will be used to
+// render the Blazor application onto the DOM each time it is mounted.
+let appTemplate = document.createElement("template");
+
+// Reference to the Blazor runtime object used to manage the Blazor WebAssembly application and its
+// associated .NET browser runtime.
+let blazor: any;
+
+// The original caches.open function that we replace with a custom implementation that applies a
+// discriminator to the cache name. This is necessary to ensure that each Blazor micro-frontend has
+// its own cache, which Blazor typically only names after the page's base URL.
+let originalCachesOpen: (cacheName: string) => Promise<Cache>;
+
+export default function singleSpaBlazor<ExtraProps>(
+  blazorOptions: BlazorWebAssemblyOptions<ExtraProps>
+): BlazorWebAssemblyLifeCycles<ExtraProps> {
+  if (typeof blazorOptions !== "object") {
+    throw new Error(`single-spa-blazor-wasm requires a configuration object`);
+  }
+
+  if (!blazorOptions.appTagName) {
+    throw new Error(`single-spa-blazor-wasm must be given customElementTagName`);
+  }
+
+  if (blazorOptions.domElementGetter && typeof blazorOptions.domElementGetter !== "function") {
+    throw new Error(`single-spa-blazor-wasm domElementGetter must be a function`);
+  }
+
+  // TypeScript cannot infer the type of Function.prototype.bind when the partially applied
+  // arguments are of a generic type. Thus, we need to explicitly specify the type parameters for
+  // bind, which turns out there are four in total.
+  //
+  // The first is the type of the this object, which we don't care about. We set the context of the
+  // bound function to null.
+  //
+  // The second is the type of the arguments that we are partially applying to the bound function.
+  // In this case, it's the BlazorWebAssemblyOptions.
+  //
+  // The third is the type of the arguments that will be passed to our bound function. In this case,
+  // its the set of props passed by single-spa.
+  //
+  // The final is the return type of the bound function.
+  type LifeCycleHelperFn = (
+    o: BlazorWebAssemblyOptions<ExtraProps>,
+    p: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+  ) => ReturnType<BlazorWebAssemblyLifeCycleFn<ExtraProps>>;
+
+  type This = null;
+  type HelperParameters = Parameters<LifeCycleHelperFn>;
+  type Options = [HelperParameters[0]];
+  type Props = [HelperParameters[1]];
+  type Result = ReturnType<LifeCycleHelperFn>;
+
+  return {
+    bootstrap: bootstrap.bind<This, Options, Props, Result>(null, blazorOptions),
+    mount: mount.bind<This, Options, Props, Result>(null, blazorOptions),
+    unmount: unmount.bind<This, Options, Props, Result>(null, blazorOptions),
+  };
+}
+
+//
+// The actual single-spa lifecycle callbacks.
+//
+
+// NOTE: We don't do any bootstrapping. Ideally we would load the .NET and Blazor runtimes here, but
+// the Blazor startup scripts couple the loading of the runtimes with the actual start and execution
+// with of them. This causes any global .NET and Blazor runtime objects to be overwritten.
+//
+// This is particularly relevant if we have a Blazor micro-frontend currently on the page that is
+// unmounting and another one that wants to bootstrap and mount. In this case, we need to make sure
+// all Razor components from the first one are fully removed from the page and disposed before the
+// second one tries to boot and load its version of the .NET runtime and Blazor. Otherwise, you may
+// have the first micro-frontend calling into the runtime of the second micro-frontend, which will
+// create a slew of runtime errors.
+//
+// Since single-spa will let a micro-frontend bootstrap concurrently with another one that is
+// unmounting, bootstrap would not be an safe place to load the second Blazor micro-frontend's
+// runtime. Instead, we do that in the mount lifecycle hook because single-spa will ensure that all
+// unmount lifecycle hooks are resolved before any mount lifecycle hooks are called.
+function bootstrap<ExtraProps>(
+  _blazorOptions: BlazorWebAssemblyOptions<ExtraProps>,
+  _props: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+): ReturnType<BlazorWebAssemblyLifeCycleFn<ExtraProps>> {
+  return Promise.resolve();
+}
+
+// On the first mount, the Blazor micro-frontend will fetch and import its Blazor startup script.
+// As a side effect, the .NET and Blazor runtime objects will be written to the window. Then, the
+// Blazor runtime and application are started. This will fetch the micro-frontend's assets and load
+// them in the browser.
+//
+// This also installs an interceptor for the caches.open function. The .NET browser runtime uses
+// this to open a named cache for caching .NET assets. Unfortunately, the name of the cache is not
+// configurable and is based (currently) on the page's base URL, which isn't always unique enough,
+// especially in the case of micro-frontends. The original caches.open function is saved so that it
+// be restored when the micro-frontend is unmounted.
+//
+// Once loaded and started, the Blazor micro-frontend will be mounted to the DOM.
+async function mount<ExtraProps>(
+  blazorOptions: BlazorWebAssemblyOptions<ExtraProps>,
+  props: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+): ReturnType<BlazorWebAssemblyLifeCycleFn<ExtraProps>> {
+  const { name: appName } = props;
+  const extensions = getExtensions(blazorOptions);
+
+  const assetBaseUrl =
+    props.assetBaseUrl || blazorOptions.assetBaseUrl || new URL(document.baseURI);
+
+  const navigationBaseUrl =
+    props.navigationBaseUrl || blazorOptions.navigationBaseUrl || new URL(document.baseURI);
+
+  // Install an interceptor for caches.open so that we can apply a discriminator to the named
+  // cache the .NET browser runtime will user for caching .NET assets.
+  originalCachesOpen = globalThis.caches.open;
+  globalThis.caches.open = function (cacheName) {
+    return originalCachesOpen.call(globalThis.caches, `${cacheName}-${appName}`);
+  };
+
+  if (blazor === undefined) {
+    // On first mount, import the Blazor runtime for the Blazor micro-frontend. Capture the Blazor
+    // runtime reference from the window after it has been imported.
+    await import(new URL("_framework/blazor.webassembly.js", assetBaseUrl).href);
+    // @ts-expect-error (ts7015) - We don't have a better type than any for the Blazor object.
+    blazor = window["Blazor"];
+
+    // Import any additional scripts that need to be loaded before the Blazor runtime and
+    // application are started.
+    await Promise.all(
+      extensions.flatMap((extension) =>
+        !extension.additionalImportPaths
+          ? Promise.resolve()
+          : extension.additionalImportPaths.map((path) => import(new URL(path, assetBaseUrl).href))
+      )
+    );
+
+    // Run any before start callbacks that have been defined.
+    for (const extension of extensions) {
+      if (extension.beforeBlazorStart) {
+        await Promise.resolve(extension.beforeBlazorStart(assetBaseUrl));
+      }
+    }
+
+    // Produce the template content used to mount the application.
+    const styleElements = extensions.flatMap((extension) =>
+      !extension.stylePaths
+        ? []
+        : extension.stylePaths.map((stylePath) => {
+            const styleElement = document.createElement("link");
+            styleElement.rel = "stylesheet";
+            styleElement.href = new URL(stylePath, assetBaseUrl).href;
+            return styleElement;
+          })
+    );
+
+    appTemplate.content.append(...styleElements, document.createElement(blazorOptions.appTagName));
+
+    if (blazorOptions.injectRefreshScript) {
+      // If debugging is enabled, then we need to inject the ASP.NET Core browser refresh script.
+      const browserRefreshScript = document.createElement("script");
+      browserRefreshScript.src = new URL(
+        "_framework/aspnetcore-browser-refresh.js",
+        assetBaseUrl
+      ).href;
+      appTemplate.content.append(browserRefreshScript);
+    }
+
+    // Start the Blazor runtime and its application.
+    await blazor.start({
+      // NOTE: These are critical for integration. They control how Blazor will resolve relative and
+      // absolute URL paths for assets and navigation. Without them, Blazor will tend to resolve
+      // these relative to the page's base URL, which is not what we want in the case of a Blazor
+      // micro-frontend.
+      assetBaseUrl,
+      navigationBaseUrl,
+
+      // Define a custom boot resource loader that will apply the module base URL to any URL paths
+      // (relative or absolute) the .NET loader tries resolve.
+      loadBootResource: function (
+        _type: unknown,
+        _name: string,
+        defaultUrl: string,
+        _integrity: string,
+        _behavior: unknown
+      ) {
+        return new URL(defaultUrl, assetBaseUrl).href;
+      },
+
+      // Additional configuration of the .NET host used to run the Blazor application.
+      configureRuntime: (dotnet: any) => {
+        if (blazorOptions.configureRuntime) {
+          blazorOptions.configureRuntime(dotnet, props);
+        }
+      },
+    });
+  } else {
+    // After the first mount, restore global Blazor state on the window and ensure Blazor's DOM
+    // listeners are reconnected and ready.
+    blazor.restoreGlobalState();
+    blazor.ensureDomListenersReady();
+
+    // Run any after restore callbacks that have been defined.
+    for (const appExtension of extensions) {
+      if (appExtension.afterBlazorRestore) {
+        await Promise.resolve(appExtension.afterBlazorRestore(blazor));
+      }
+    }
+  }
+
+  // Force the Blazor navigation manager to resync with the page's current URL before mounting the
+  // application.
+  blazor.navigateTo(document.location.href, {
+    replaceHistoryEntry: true,
+    forceLoad: false,
+  });
+
+  // Mount the Blazor application on the DOM. This depends on Blazor's ability to register a custom
+  // element with the browser's custom elements registry. When the custom element is connected to
+  // the DOM, that will trigger Blazor to add the application's root component (typically called
+  // App) onto the page.
+  //
+  // NOTE: This will require configuration on a per micro-frontend basis. Things like the name of
+  // the custom element's tag, any scripts or styles that need to be loaded, etc.
+  const domElementGetter = chooseDomElementGetter(blazorOptions, props);
+  const mountPoint = domElementGetter(props);
+  if (mountPoint) {
+    mountPoint.replaceChildren(appTemplate.content.cloneNode(true));
+  }
+}
+
+async function unmount<ExtraProps>(
+  blazorOptions: BlazorWebAssemblyOptions<ExtraProps>,
+  props: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+): ReturnType<BlazorWebAssemblyLifeCycleFn<ExtraProps>> {
+  const extensions = getExtensions(blazorOptions);
+  const domElementGetter = chooseDomElementGetter(blazorOptions, props);
+  const mountPoint = domElementGetter(props);
+
+  if (mountPoint) {
+    // Disconnect the Blazor application from the DOM.
+    mountPoint.replaceChildren();
+
+    // Ensure the Blazor micro-frontend has disposed its components before proceeding. Then remove
+    // any event listeners that Blazor has attached to the DOM and cleanup the global state added
+    // by Blazor.
+    await blazor.ensureRazorComponentsDisposed();
+    blazor.ensureDomListenersRemoved();
+    blazor.clearGlobalState();
+
+    // Run any after clear callbacks that have been defined.
+    for (const extension of extensions) {
+      if (extension.afterBlazorClear) {
+        await Promise.resolve(extension.afterBlazorClear(blazor));
+      }
+    }
+  }
+
+  // Restore the original caches.open function.
+  globalThis.caches.open = originalCachesOpen;
+}
+
+function getExtensions<ExtraProps>(blazorOptions: BlazorWebAssemblyOptions<ExtraProps>) {
+  // NOTE: Apply the micro-frontend's hooks after any additional extensions defined on the given
+  // set of Blazor WebAssembly options.
+  return [
+    ...(blazorOptions.appExtensions ?? []),
+    {
+      stylePaths: blazorOptions.stylePaths,
+      additionalImportPaths: blazorOptions.additionalImportPaths,
+      beforeBlazorStart: blazorOptions.beforeBlazorStart,
+      afterBlazorRestore: blazorOptions.afterBlazorRestore,
+      afterBlazorClear: blazorOptions.afterBlazorClear,
+    },
+  ];
+}
+
+// The following is a common pattern in other single-spa framework helpers for picking the DOM
+// element onto which the micro-frontend is mounted.
+function chooseDomElementGetter<ExtraProps>(
+  blazorOptions: BlazorWebAssemblyOptions<ExtraProps>,
+  props: Parameter<BlazorWebAssemblyLifeCycleFn<ExtraProps>>
+): (props: AppProps) => HTMLElement {
+  const domElementFromProps = props.domElement;
+
+  if (domElementFromProps) {
+    return () => domElementFromProps;
+  } else if (props.domElementGetter) {
+    return props.domElementGetter;
+  } else if (blazorOptions.domElementGetter) {
+    return blazorOptions.domElementGetter;
+  } else {
+    return defaultDomElementGetter(props);
+  }
+}
+
+function defaultDomElementGetter(props: AppProps) {
+  if (!props.name) {
+    throw new Error(`single-spa-blazor-wasm was not given an application name as a prop`);
+  }
+
+  const htmlId = `single-spa-application:${props.name}`;
+
+  return function defaultDomElement() {
+    let domElement = document.getElementById(htmlId);
+
+    if (!domElement) {
+      domElement = document.createElement("div");
+      domElement.id = htmlId;
+      document.body.appendChild(domElement);
+    }
+
+    return domElement;
+  };
+}
